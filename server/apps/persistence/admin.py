d7c1b564b9c84fd35c217e5989a3623f24459d6f
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2026-02-23 21:59:51 -0300

    chore: refactor admin model registrations

--- server/apps/persistence/admin.py
+++ server/apps/persistence/admin.py
@@ -3,19 +3,15 @@ from apps.persistence.models.songs import Category, Song, Played
 from apps.persistence.models.profile import Profile
 from apps.persistence.models.schedule import ScheduleType, MemberScheduleConfig, MonthlySchedule
 from apps.persistence.models.hymnal import Hymn
-from apps.persistence.models.member import Member
+from apps.persistence.models.member import Member,MemberStatus,Role,Ministry
 from apps.persistence.models.gallery import Album, Photo
 
-admin.site.register(Category)
-admin.site.register(Song)
-admin.site.register(Played)
 
-admin.site.register(Member)
-admin.site.register(Profile)
-admin.site.register(ScheduleType)
-admin.site.register(MemberScheduleConfig)
-admin.site.register(MonthlySchedule)
+models = [
+    Category, Song, Played, Profile, ScheduleType,
+    MemberScheduleConfig, MonthlySchedule, Hymn,
+    Album, Photo, Member, MemberStatus, Role, Ministry
+]
 
-admin.site.register(Hymn)
-admin.site.register(Album)
-admin.site.register(Photo)
+for model in models:
+    admin.site.register(model)
