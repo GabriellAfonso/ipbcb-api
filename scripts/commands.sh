addca0194e72acb2d46147630e3cca519ea1f89c
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2026-02-24 19:35:21 -0300

    chore: switch to gunicorn server

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -15,5 +15,10 @@ python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 sleep 0.2
 
-python manage.py runserver 0.0.0.0:8000
-#gunicorn config.wsgi:application --bind 0.0.0.0:8000
+#python manage.py runserver 0.0.0.0:8000
+gunicorn config.wsgi:application \
+    --bind 0.0.0.0:8000 \
+    --workers 3 \
+    --access-logfile - \
+    --error-logfile - \
+    --log-level info
