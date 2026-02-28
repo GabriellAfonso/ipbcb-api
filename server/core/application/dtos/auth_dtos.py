021ef646af9f3d479a6ee41f55bdf3cf38c70a15
Gabriel Afonso <gabrieldelimaafonso@gmail.com>
2026-02-28 20:33:12 -0300

    fix: update RegisterDTO minimum password length

--- server/core/application/dtos/auth_dtos.py
+++ server/core/application/dtos/auth_dtos.py
@@ -6,7 +6,7 @@ from pydantic import EmailStr
 
 class RegisterDTO(StrictBaseModel):
     username: str = Field(..., min_length=1, max_length=150)
-    password: str = Field(..., min_length=8)
+    password: str = Field(..., min_length=6)
     first_name: str = Field(..., min_length=1, max_length=50)
     last_name: str = Field(..., min_length=1, max_length=50)
 
